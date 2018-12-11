2f18821a93d70d20950d1c361e52a658390d59a0
Николай <rudopisovnick@hotmail.com>
2018-12-11 15:22:59 +0400

    Изменил форматирование.

--- src/main/java/com/parkinghelper/parker/repositories/ParkingPlaceRepository.java
+++ src/main/java/com/parkinghelper/parker/repositories/ParkingPlaceRepository.java
@@ -1,9 +1,7 @@
-package com.parkinghelper.parker.Repositories;
+package com.parkinghelper.parker.repositories;
 
 import com.parkinghelper.parker.domain.ParkingPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface ParkingPlaceRepository extends JpaRepository<ParkingPlace,Long> {
 
