83444f02526279617fe0f806b63b84ac26192361
Николай <rudopisovnick@hotmail.com>
2019-01-12 23:48:28 +0400

    Use again lambok

--- src/main/java/com/parkinghelper/parker/domain/ParkingGeoArea.java
+++ src/main/java/com/parkinghelper/parker/domain/ParkingGeoArea.java
@@ -3,6 +3,8 @@ package com.parkinghelper.parker.domain;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.parkinghelper.parker.domain.types.Zone;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
@@ -15,67 +17,38 @@ public class ParkingGeoArea {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
     private Long id;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "address")
+    @Getter
+    @Setter
     private AreaGeoAddress geoAddress;
 
     @Column(name = "free_space_count")
+    @Getter
+    @Setter
     private Integer freeSpaceCount;
 
     @NotNull
     @Column(name = "zone_coordinate", columnDefinition = "box")
     @Type(type = "com.parkinghelper.parker.domain.types.BoxUserType")
+    @Getter
+    @Setter
     private Zone zoneCoordinate;
 
     @OneToMany(mappedBy = "area",
             cascade = CascadeType.REMOVE,
             fetch = FetchType.EAGER
     )
+    @Setter
     private Set<ParkingPlace> Places;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public AreaGeoAddress getGeoAddress() {
-        return geoAddress;
-    }
-
-    public void setGeoAddress(AreaGeoAddress geoAddress) {
-        this.geoAddress = geoAddress;
-    }
-
-    public Integer getFreeSpaceCount() {
-        return freeSpaceCount;
-    }
-
-    public void setFreeSpaceCount(Integer freeSpaceCount) {
-        this.freeSpaceCount = freeSpaceCount;
-    }
-
-    public Zone getZoneCoordinate() {
-        return zoneCoordinate;
-    }
-
-    public void setZoneCoordinate(Zone zoneCoordinate) {
-        this.zoneCoordinate = zoneCoordinate;
-    }
 
     @JsonManagedReference
     public Set<ParkingPlace> getPlaces() {
         return Places;
-    }
-
-    public void setPlaces(Set<ParkingPlace> places) {
-        Places = places;
-    }
-
-    public ParkingGeoArea() {
     }
 }
--- src/main/java/com/parkinghelper/parker/domain/ParkingPlace.java
+++ src/main/java/com/parkinghelper/parker/domain/ParkingPlace.java
@@ -1,6 +1,8 @@
 package com.parkinghelper.parker.domain;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.Type;
 import org.postgresql.geometric.PGpoint;
 
@@ -14,53 +16,30 @@ public class ParkingPlace {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
     private Long id;
 
     @NotNull
     @Column(name = "coordinate", columnDefinition = "Point")
     @Type(type = "com.parkinghelper.parker.domain.types.PointUserType")
+    @Getter
+    @Setter
     private PGpoint coordinate;
 
     @JoinColumn(name = "is_free")
+    @Getter
+    @Setter
     private Boolean isFree;
 
     @ManyToOne
     @JoinColumn(name = "area")
+    @Setter
     private ParkingGeoArea area;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public PGpoint getCoordinate() {
-        return coordinate;
-    }
-
-    public void setCoordinate(PGpoint coordinate) {
-        this.coordinate = coordinate;
-    }
-
-    public Boolean getIsFree() {
-        return isFree;
-    }
-
-    public void setIsFree(Boolean free) {
-        isFree = free;
-    }
 
     @JsonBackReference
     public ParkingGeoArea getArea() {
         return area;
-    }
-
-    public void setArea(ParkingGeoArea area) {
-        this.area = area;
-    }
-
-    public ParkingPlace() {
     }
 }
